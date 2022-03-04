db5b2cb80f801d16e8252ced1e4e606d7f78ce37
Lucas Renó <lucasreno9@gmail.com>
2022-03-03 22:18:17 -0300

    App criado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  dado: string = '';
+  sorteando: boolean = false;
+  constructor() { }
 
-  constructor() {}
 
+  async sortear() {
+    for (let i = 0; i < 10; i++) {
+      this.sorteando = true;
+      let sorteio = Math.random();
+      sorteio = Math.floor(sorteio * 6 + 1);
+      this.dado = sorteio.toString();
+      await new Promise(f => setTimeout(f, 100));
+    }
+    this.sorteando = false;
+  }
 }
